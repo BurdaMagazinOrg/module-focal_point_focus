062ae478352eca96a7c7123286b35969b0b29c26
Georg Oechsler <go@wunderkraut.com>
2016-08-25 12:49:43 +0200

    DRPLDCX-89 Allow saving of crop rectangle coordinates alongside focus settings

---
+++ src/Plugin/Field/FieldWidget/FocalPointFocusImageWidget.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\focal_point_focus\Plugin\Field\FieldWidget\FocalPointFocusImageWidget.
+ */
+
+namespace Drupal\focal_point_focus\Plugin\Field\FieldWidget;
+
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\crop\Entity\Crop;
+use Drupal\focal_point\Plugin\Field\FieldWidget\FocalPointImageWidget;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\image\Plugin\Field\FieldWidget\ImageWidget;
+
+/**
+ * Extension of focal_point's FocalPointImageWidget adding a crop area.
+ *
+ * @see focal_point_focus_field_widget_form_alter
+ */
+class FocalPointFocusImageWidget extends FocalPointImageWidget {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function process($element, FormStateInterface $form_state, $form) {
+    $element = parent::process($element, $form_state, $form);
+
+
+    $element['crop_rect'] = [
+      '#type' => 'textfield',
+      '#title' => t('Crop rectangle'),
+      '#element_validate' => [[__CLASS__, 'validateCropRectFormat']],
+      '#description' => t('Coordinates of the crop rectangle "x1,y1,x2,y2".')
+    ];
+    $element['#element_validate'][] = [__CLASS__, 'validateValues'];
+
+    return $element;
+  }
+
+  /**
+   * Validation callback enforcing the input formally.
+   */
+  public static function validateCropRectFormat($element, FormStateInterface $form_state) {
+    $crop_rect = $element['#value'];
+    if (empty($crop_rect)) {
+      return;
+    }
+
+    if (!preg_match('#^\d+,\d+,\d+,\d+$#', $crop_rect)) {
+      $form_state->setError($element, new TranslatableMarkup('%title needs to be either empty or four non-negative integers separated by comma.', array('%title' => $element['#title'])));
+    }
+
+    $crop_coords = explode(',', $crop_rect);
+    if (
+      $crop_coords[0] >= $crop_coords[2] || // Left is greater than right
+      $crop_coords[1] >= $crop_coords[3] // Upper is greater than lower
+    ) {
+      $form_state->setError($element, new TranslatableMarkup('The first two value need to describe the upper left corner of the crop rectangle.'));
+    }
+  }
+
+  /**
+   * Validation callback checking for value sanity such as the focal point being
+   * inside the crop rectangle and the crop rectangle being inside the image
+   * boundaries.
+   */
+  public static function validateValues($element, FormStateInterface $form_state) {
+    if (empty($element['#value']['crop_rect'])) {
+      return;
+    }
+
+    $crop_rect = explode(',', $element['#value']['crop_rect']);
+    if (
+      $crop_rect[0] > $element['#value']['width'] || // Left is greater than image width
+      $crop_rect[1] > $element['#value']['height'] || // Upper is greater than image height
+      $crop_rect[2] > $element['#value']['width'] || // Right is greater than image width
+      $crop_rect[3] > $element['#value']['height'] // Lower is greater than image height
+    ) {
+      $form_state->setError($element, new TranslatableMarkup('The crop rectangle needs to be within the image boundaries.'));
+    }
+
+    $focal_point = explode(',', $element['#value']['focal_point']);
+
+    $real_focal_point = [
+      $focal_point[0] * $element['#value']['width'] / 100,
+      $focal_point[1] * $element['#value']['height'] / 100,
+    ];
+
+    if (
+      $crop_rect[0] > $real_focal_point[0] || // Focal point is smaller then left
+      $crop_rect[1] > $real_focal_point[1] || // Focal point is smaller then upper
+      $crop_rect[2] < $real_focal_point[0] || // Focal point is greater then right
+      $crop_rect[3] < $real_focal_point[1] // Focal point is greater then lower
+    ) {
+      $form_state->setError($element, new TranslatableMarkup('The focal point needs to be inside the crop rectancle!'));
+    }
+  }
+}
---
+++ src/Plugin/ImageEffect/FocalPointFocusScaleAndCropImageEffect.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\focal_point_focus\Plugin\ImageEffect\FocalPointFocusScaleAndCropImageEffect.
+ */
+
+namespace Drupal\focal_point_focus\Plugin\ImageEffect;
+
+use Drupal\crop\Entity\Crop;
+use Drupal\focal_point\FocalPointEffectBase;
+use Drupal\Core\Image\ImageInterface;
+
+/**
+ * Scales and crops image while keeping its focal point close to centered.
+ *
+ * @ImageEffect(
+ *   id = "focal_point_focus_scale_and_crop",
+ *   label = @Translation("Focal Point Focus Scale and Crop"),
+ *   description = @Translation("Scales and crops like focal point, but crops once before doing so.")
+ * )
+ */
+class FocalPointFocusScaleAndCropImageEffect extends FocalPointEffectBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applyEffect(ImageInterface $image) {
+    // Hold on to the original width and height. We'll need it later.
+    $image_size = [
+      'width' => $image->getWidth(),
+      'height' => $image->getHeight(),
+    ];
+
+    // First, attempt to resize the image.
+    $resize_data = self::calculateResizeData($image_size['width'], $image_size['height'], $this->configuration['width'], $this->configuration['height']);
+    if (!$image->resize($resize_data['width'], $resize_data['height'])) {
+      $this->logger->error(
+        'Focal point scale and crop failed while resizing using the %toolkit toolkit on %path (%mimetype, %dimensions)',
+        [
+          '%toolkit' => $image->getToolkitId(),
+          '%path' => $image->getSource(),
+          '%mimetype' => $image->getMimeType(),
+          '%dimensions' => $image->getWidth() . 'x' . $image->getHeight(),
+        ]
+      );
+      return FALSE;
+    }
+
+    // Next, attempt to crop the image.
+    return $this->applyCrop($image, $image_size);
+  }
+
+}
